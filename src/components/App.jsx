6b771ce61e0a511f98d06cecd242bbf7b85687b2
MOkhten <m.okhten@gmail.com>
2022-12-30 15:18:47 +0200

    phoneboook

---
+++ src/components/App.jsx
@@ -0,0 +1,64 @@
+import { nanoid } from 'nanoid';
+import { Component } from "react";
+import { ContactList } from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { Phonebook } from './App.styled';
+
+export class App  extends Component {
+    state = {
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+  filter: '',
+};
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+
+  addContact = ({ name, number }) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.setState(({ contacts, filter }) => ({
+      contacts: [newContact, ...contacts],
+      
+    }));
+    console.log(this.state);
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+
+   visibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalized = filter.toLowerCase();
+    return contacts.filter(contact => contact.name.toLowerCase().includes(normalized));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    return (
+      <Phonebook>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} contacts={contacts}/>
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange = {this.changeFilter} />
+        <ContactList
+          contacts={this.visibleContacts()}
+        onDeleteContact={this.deleteContact}/>
+     </Phonebook>
+    );
+      
+    }
+}
